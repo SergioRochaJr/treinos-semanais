00825b28df7e20aeefce7a07408fbee1abb2da23
SergioRochaJr <sergiorajr1994@gmal.com>
2026-03-04 17:48:20 -0300

    fix tests

--- tests/test_project_euler_1.py
+++ tests/test_project_euler_1.py
@@ -1,9 +1,9 @@
 from treinos_semanais.project_euler_1 import exe1
 
 
-def test_example():
+def test_example() -> None:
     assert 23 == exe1(10)
 
 
-def test_wanted():
+def test_wanted() -> None:
     assert 233_168 == exe1(1_000)
